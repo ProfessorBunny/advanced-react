ff8fa67a3f959bcfb6a42f5b25b483cf338a636e
ProfessorBuny <nikunjraghav24@gmail.com>
2025-03-31 15:52:52 +0100

    working with ternarry operator

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,28 @@
-const UserChallenge = () => {
-  return <h2>user challenge</h2>;
-};
+import { useState } from 'react'
 
-export default UserChallenge;
+const UserChallenge = () => {
+  const [user, setuser] = useState(null)
+  const login = () => {
+    setuser({ name: 'john' })
+  }
+  const logout = () => {
+    setuser(null)
+  }
+  return (
+    <div>
+      <h2>Challenge</h2>
+      <h3>{user ? `Hello ${user.name}` : 'Please log in'}</h3>
+      {user ? (
+        <button className="btn" onClick={logout}>
+          Logout
+        </button>
+      ) : (
+        <button className="btn" onClick={login}>
+          Login
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default UserChallenge
